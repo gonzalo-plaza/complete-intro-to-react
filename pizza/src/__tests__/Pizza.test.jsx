ccfc647f0cd89b1554ab00164ce58a70376454a5
gonzalo-plaza <plazaruedag@gmail.com>
2025-12-21 17:48:52 +0100

    complete-creact-v9-course: Commit until Test-Driven Development on a Component lesson.

---
+++ pizza/src/__tests__/Pizza.test.jsx
@@ -0,0 +1,28 @@
+import { render, cleanup } from "@testing-library/react";
+import { expect, test } from "vitest";
+import Pizza from "../Pizza";
+import { afterEach } from "vitest";
+
+afterEach(cleanup)
+
+test("alt test renders on Pizza image", async () =>  {
+  const name = "My Favourite Pizza";
+  const src = "https://picsum.photos/200";
+
+  const screen = render(
+    <Pizza name={name} description="super cool pizza" image={src} />,
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).toBe(src);
+  expect(img.alt).toBe(name);
+})
+
+test("to have default image if none is provided", async () => {
+  const screen = render(
+    <Pizza name="something else" description="super cool pizza" />,
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).not.toBe("");
+});
