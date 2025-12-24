9718104c7043e573076a2c170469653b1cd01e66
gonzalo-plaza <plazaruedag@gmail.com>
2025-12-24 12:47:50 +0100

    complete-react-v9-course: Add react compiler and fix format on vite.config.js

--- pizza/vite.config.js
+++ pizza/vite.config.js
@@ -1,19 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   server: {
     proxy: {
-      "/api":{
+      "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
       },
-      "/public":{
+      "/public": {
         target: "http://localhost:3000",
         changeOrigin: true,
       },
-    }
+    },
   },
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [
+    TanStackRouterVite(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+  ],
 });
